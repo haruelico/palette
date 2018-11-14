ceaeca20c8494d5d56c8ea30a13c8daed91bc8bb
Harue Lico <44547634+haruelico@users.noreply.github.com>
2018-11-15 02:17:49 +0900

    Activate vue pulgin vue-highlightjs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueHighlightJS from "vue-highlightjs";
 
 Vue.config.productionTip = false;
+Vue.use(VueHighlightJS);
 
 new Vue({
   render: h => h(App)
