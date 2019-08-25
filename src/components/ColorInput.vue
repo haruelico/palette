d35c780c04b2e9bfdeea834b145a262288f10f72
Boda-Kosuzu <boda.kosuzu@gmail.com>
2019-08-25 19:30:31 +0900

    赤ペン先生

---
+++ src/components/ColorInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <tr>
+    <td>
+      <input
+        :id="inputId"
+        v-model="color"
+        type="color"
+      >
+    </td>
+    <td>
+      <label class="style-name">{{ styleName }}</label>
+    </td>
+    <td>
+      <label>{{ colorCode }}</label>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'ColorInput',
+  props: {
+    colorCode: {
+      type: String,
+      required: true
+    },
+    styleName: {
+      type: String,
+      required: true
+    },
+    inputId: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    color: {
+      get(){
+        return this.colorCode
+      },
+      set(color){
+        this.$emit('updateColor', {
+          color, 
+          key: this.inputId
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.style-name {
+  padding-left: 5px;
+}
+</style>
